282318338b3550380edef7dff5973c1b2bb55d42
vanhnguyen204 <nguyenvietem2004@gmail.com>
2023-05-09 12:59:50 +0700

    Initial commit

---
+++ app/src/main/java/com/fpoly/lab1_ph32739/MainActivity.java
@@ -0,0 +1,14 @@
+package com.fpoly.lab1_ph32739;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
